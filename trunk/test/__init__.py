264ebfd11bbd87090d14ea05e239bfe605bf7093
mikepan <mikepan@f3d7b266-6c56-11de-9a7c-6184c298a20a>
2010-02-10 06:13:24 +0000

    * modifications to support use of jsonpickle instead * explicitly pickle the curried function, because jsonpickle cannot handle it

--- trunk/test/__init__.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-import TestCurry
-
-def additional_tests():
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(TestCurry.TestCase, 'test'))
-    return suite
-
